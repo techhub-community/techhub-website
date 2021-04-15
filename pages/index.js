064d770ccaa916a1bc3f75a7ed3f25f0bfa7bb24
dntandan <deepnarayan006@gmail.com>
2021-04-15 11:36:42 +0530

    made alignment center in mobile view

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,10 @@ export default function Home() {
           <img className="text-center my-12 w-80" src="/assets/logo/logo-full-transparent.png"/>
         </a>
       </div>
-      <div className="flex flex-row justify-center gap-4 text-center">
+      <div className="flex flex-col md:flex-row items-center gap-4 text-center">
         <a href="https://discord.com/invite/EMEMejk8P5"><img src="https://img.shields.io/discord/814219041614594078?label=Join%20Us%20-%20Discord" alt=""/></a>
         <a href="mailto:hello@tech-hub.org"><img src="https://img.shields.io/badge/Contact%20Us-hello%40tech--hub.org-orange" alt=""/></a>
-        <img src="https://img.shields.io/github/stars/techhub-community?label=Github%20Stars&amp;style=plastic" alt="GitHub Org&#39;s stars"/>
+        <a href="https://github.com/techhub-community"> <img src="https://img.shields.io/github/stars/techhub-community?label=Github%20Stars&amp;style=plastic" alt="GitHub Org&#39;s stars"/></a>
       </div>
 
       <div className={styles.main}>
