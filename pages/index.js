3ad3d4b5847551dad635f8e44bb5c4e5fbf47bf7
dntandan <deepnarayan006@gmail.com>
2021-04-15 12:08:02 +0530

    added opengrpah and meta tags data

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,24 @@ export default function Home() {
       <Head>
         <title>TechHub Community</title>
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Meta Tags */}
+        <meta name="title" content="TechHub :: Community" key="title"/>
+        <meta name="description" key="description" content="At TechHub, we've developed a huge community around building programming challenges, exciting real-world projects in every stack, and daily discussions. There are over 250 people who love solving these problems and helping others do."/>
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" key="ogtype" content="website"/>
+        <meta property="og:url" key="ogurl" content="https://tech-hub.org/"/>
+        <meta property="og:title" key="ogtitle" content="TechHub :: Community"/>
+        <meta property="og:description" key="ogdescription" content="At TechHub, we've developed a huge community around building programming challenges, exciting real-world projects in every stack, and daily discussions. There are over 250 people who love solving these problems and helping others do."/>
+        <meta property="og:image" key="ogimage" content="https://tech-hub.org/assets/logo/logo-square.png"/>
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" key="twittercard" content="summary_large_image"/>
+        <meta property="twitter:url" key="twitterurl" content="https://tech-hub.org/"/>
+        <meta property="twitter:title" key="twittertitle" content="TechHub :: Community"/>
+        <meta property="twitter:description" key="twitterdesc" content="At TechHub, we've developed a huge community around building programming challenges, exciting real-world projects in every stack, and daily discussions. There are over 250 people who love solving these problems and helping others do."/>
+        <meta property="twitter:image" key="twitterimage" content="https://tech-hub.org/assets/logo/logo-square.png"/>
       </Head>
       <div className="text-center">
         <a href="https://github.com/techhub-community" target="_blank">
