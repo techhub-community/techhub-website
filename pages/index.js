9ae110396070ed82cca0f945193416f6a1917270
Sekhar Jha <sekharjha14@gmail.com>
2021-04-18 23:25:28 +0530

    Fix Imports

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import React, { useState, useEffect } from 'react';
 import Navbar from "../components/Navbar";
-import Dropdown from "../components/dropdown";
+import Dropdown from "../components/Dropdown";
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
 
