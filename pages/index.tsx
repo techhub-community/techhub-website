3a8b7c08b43eec7aa9f856790ee630f3311e6ba3
Sekhar Jha <sekharjha14@gmail.com>
2021-04-18 23:25:28 +0530

    Fix Imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 import React, { useState, useEffect } from 'react';
 import Navbar from "../components/Navbar";
-import Dropdown from "../components/dropdown";
+import Dropdown from "../components/Dropdown";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
