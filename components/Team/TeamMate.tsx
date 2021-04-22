606d62d4e0401de20bbb314393b8749c12c99263
dntandan <deepnarayan006@gmail.com>
2021-04-22 19:38:03 +0530

    data is being passed as props in team component

--- components/Team/TeamMate.tsx
+++ components/Team/TeamMate.tsx
@@ -1,28 +1,64 @@
 import React from 'react';
 import {
+  IoGlobeOutline,
   IoLogoFacebook,
   IoLogoGithub,
   IoLogoInstagram,
   IoLogoLinkedin,
+  IoLogoTwitter,
 } from 'react-icons/io5';
 
-export default function TeamMate() {
+export default function TeamMate(props) {
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
-        <img
-          className="rounded-full"
-          src="https://picsum.photos/200"
-          alt="team 1"
-        />
-        <h2 className="text-xl font-medium mt-6">John Doe</h2>
-        <h3 className="text-md">john.doe@gmail.com</h3>
+      <div className="flex flex-col items-center justify-center text-center">
+        <img className="rounded-full h-36 w-36" src={props.img} alt="team 1" />
+        <h2 className="text-xl font-medium mt-6">{props.name}</h2>
+        <h3 className="text-md">{props.designation}</h3>
         <div className="w-full flex justify-center pt-5 pb-5">
           <div className="flex flex-row gap-6 place-items-center mr-4">
-            <IoLogoFacebook className="text-2xl text-black-700 hover:text-brand-500" />
-            <IoLogoInstagram className="text-2xl text-black-700 hover:text-brand-500" />
-            <IoLogoLinkedin className="text-2xl text-black-700 hover:text-brand-500" />
-            <IoLogoGithub className="text-2xl text-black-700 hover:text-brand-500" />
+            {props.github ? (
+              <a href={props.github}>
+                <IoLogoGithub className="text-2xl text-black-700 hover:text-brand-500" />
+              </a>
+            ) : (
+              ''
+            )}
+            {props.twitter ? (
+              <a href={props.twitter}>
+                <IoLogoTwitter className="text-2xl text-black-700 hover:text-brand-500" />
+              </a>
+            ) : (
+              ''
+            )}
+            {props.fb ? (
+              <a href={props.fb}>
+                <IoLogoFacebook className="text-2xl text-black-700 hover:text-brand-500" />
+              </a>
+            ) : (
+              ''
+            )}
+            {props.insta ? (
+              <a href={props.insta}>
+                <IoLogoInstagram className="text-2xl text-black-700 hover:text-brand-500" />
+              </a>
+            ) : (
+              ''
+            )}
+            {props.lin ? (
+              <a href={props.lin}>
+                <IoLogoLinkedin className="text-2xl text-black-700 hover:text-brand-500" />
+              </a>
+            ) : (
+              ''
+            )}
+            {props.web ? (
+              <a href={props.web}>
+                <IoGlobeOutline className="text-2xl text-black-700 hover:text-brand-500" />
+              </a>
+            ) : (
+              ''
+            )}
           </div>
         </div>
       </div>
