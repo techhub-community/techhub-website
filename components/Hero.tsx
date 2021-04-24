290912054246462025990cda2d53effa5d9f24db
dntandan <deepnarayan006@gmail.com>
2021-04-24 20:05:10 +0530

    hero text updated and button updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,11 +6,10 @@ export default function Hero() {
     <section className="container px-4 pt-20 xl:pt-24 mx-auto">
       <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
         <h1 className="mb-6  lg:text-7xl text-5xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
-          The only{' '}
-          <span className="block w-full text-transparent bg-clip-text bg-gradient-to-r from-brand-600 via-brand-700 to-brand-600 lg:inline">
-            Dev Community
+          This is your place. <br />
+          <span className="block w-full text-transparent bg-clip-text bg-gradient-to-br from-brand-600 via-brand-500 to-brand-700 lg:inline">
+            Explore. Learn. Build. Grow.
           </span>{' '}
-          you will ever need to grow.
         </h1>
         <p className="px-0 mb-6 text-lg text-gray-600 md:text-xl lg:px-24 ">
           At TechHub, we've developed a huge community around solving
@@ -19,9 +18,7 @@ export default function Hero() {
         </p>
         <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
           <Link href="/about">
-            <button className="mx-2 my-2 transition duration-150 ease-in-out hover:border-brand-700 hover:text-brand-600 rounded border border-black-600 text-black-500 px-6 py-2 text-md">
-              Learn More
-            </button>
+            <button className="btn text-white btn-grad">Learn More</button>
           </Link>
           {/* <button
               className="rounded-sm md:inline-flex px-5 py-2 border-solid border border-black"
