f0156d3908336329eaa6ccc2f8ecee055d66dbc6
dntandan <deepnarayan006@gmail.com>
2021-06-17 13:07:55 +0530

    404 error page modified with responsive behaviour

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,20 +1,26 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Custom404() {
- return (
-     <>
-      <div className="w-9/12 m-auto py-8 min-h-screen flex items-center justify-center">
-	<Image src="/assets/png/404.jpg" alt="404 page image" width={1500} height={1000}
-   	 className="shadow overflow-hidden sm:rounded-lg pb-28 shadow-inner absolute md:w-32 lg:w-48" />
-	<div className="border-0 border-white-200 text-center pt-8 absolute">
-     	<h1 className="text-6xl font-medium py-8 mt-72 text-pink-300">oops! Page not found</h1>
-     	<p className="text-2xl pb-8 px-12 font-medium">Oops! The page you are looking for does not exist. It might have been moved or deleted.</p>
-	<button className="bg-gradient-to-r from-pink-600 to-pink-900 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6">
-  	<Link href="/">HOME</Link>
-	</button>
-	</div>
-	</div> 
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center self-center relative min-h-screen">
+        <img
+          src="assets/svg/404.svg"
+          alt="404 image"
+          className="w-full lg:w-7/12"
+        />
+        <div className="text-center absolute lg:bottom-16">
+          <h1 className="md:text-3xl font-medium py-8 mt-72 text-gray-700 capitalize">
+            Looks like you are lost!
+          </h1>
+
+          <Link href="/">
+            <a className="w-max btn btn-border-grad text-brand-700">
+              Go Back To Home
+            </a>
+          </Link>
+        </div>
+      </div>
     </>
-     )
+  );
 }
