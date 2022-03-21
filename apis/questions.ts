4526279e7cd87b8ce67e617d89034befded6a8f2
Deep Narayan Tandan <deepnarayan006@gmail.com>
2022-03-21 13:21:56 +0530

    updated question api endpoint with new domain

--- apis/questions.ts
+++ apis/questions.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function getQuestions() {
   var response = await axios
-    .get('https://questions.tech-hub.org/data.json')
+    .get('https://questions.techhub.org.in/data.json')
     .then((response) => {
       return response.data;
     })
